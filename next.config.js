2c108a24d40855395391a9bb863f86c3387d6afb
rahaf9908 <rahafimad333@gmail.com>
2025-05-06 16:28:53 +0300

    Minor updates and style improvements

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // أي إعدادات أخرى تحتاجها
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // لو عندك مشروع Next.js داخل src
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
   future: {
-    unstable_lightningcss: false, // تعطيل استخدام lightningcss
+    unstable_lightningcss: false, 
   },
 };
